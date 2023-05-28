d2f3899873fda812b2ddf1e5e3726465ee49f611
rushkofrsky <82936498+rushkofrsky@users.noreply.github.com>
2023-05-28 19:41:58 +0300

    Update Calculator.java
    1. Строки 9-15.  Добавил проверку на принадлежность чисел арабской или римской системе. Если пользователь ввел арабские и римские числа, выбрасываем исключение.
     2. Строка 94. Теперь может выдавать результат > 10 в римской системе. Расширил список римских чисел. Топорное решение. Думаю как сделать красиво.

--- TestCalcKata/src/Calculator.java
+++ TestCalcKata/src/Calculator.java
@@ -6,10 +6,17 @@ public class Calculator {
         Scanner scanner = new Scanner(System.in);
         String input = scanner.nextLine();
         System.out.println(calc(input));
+        input  = input.toUpperCase();
+        boolean isArabic = input.matches("^\\d{1,2}[\\s-+/*]\\d{1,2}$");
+        boolean isRoman = input.matches("^[VIX]{1,4}[\\s-+/*][VIX]{1,4}$");
+        if (isArabic ^ isRoman) {
+            throw new IllegalArgumentException("Оба числа должны быть римскими или арабскими");
+        }
     }
+    // Метод читает строку ввода. Если введенные числа соответсвуют регулярным выражениям и арабского и римского формата, выбрасывает исключение
+    //"Оба числа должны быть римскими или арабскими"
+    // После обработки возвращает и выводит на экран результат.
 
-    // Метод читает строку ввода и передает введенные значения в метод calc.
-    // После обработки возвращает результат и выводит на экран результат.
 
     public static String calc(String input) {
         String[] tokens = input.trim().split("\\s+");
@@ -26,6 +33,7 @@ public class Calculator {
         int a = parseNumber(tokens[0]);
         int b = parseNumber(tokens[2]);
         char op = tokens[1].charAt(0);
+
         // Парсим массив и присваиваем значения переменным.
         // Первый токен должен быть первым целым числом, второй токен оператором (символом), третий токен вторым цылем чилсом
         if (a < 1 || a > 10 || b < 1 || b > 10) {
@@ -79,15 +87,22 @@ public class Calculator {
         }
     }
 }
-
 // Для реализации условия: Результатом операции деления является целое число, остаток отбрасывается.
 // Преобразуем строку в целое число.
 // Если это римское число, то используется метод toInt() класса RomanNumeral
 // Если преобразование не удается, выбрасываем исключение NumberFormatException "Неверный формат. Должны быть арабские или римские числа"
 enum RomanNumeral {
-    I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), IIX(8), IX(9), X(10), XL(40), L(50), XC(90), C(100);
-
-    //Список римских чисел и соответствующее этому числу, арабское число.
+    I(1), II(2), III(3), IV(4), V(5), VI(6), VII(7), VIII(8), IX(9), X(10),
+    XI(11), XII(12), XIII(13), XIV(14), XV(15), XVI(16), XVII(17), XVIII(18), XIX(19), XX(20),
+    XXI(21), XXII(22), XXIII(23), XXIV(24), XXV(25), XXVI(26), XXVII(27), XXVIII(28), XXIX(29), XXX(30),
+    XXXI(31), XXXII(32), XXXIII(33), XXXIV(34), XXXV(35), XXXVI(36), XXXVII(37), XXXVIII(38), XXXIX(39), XL(40),
+    XLI(41), XLII(42), XLIII(43), XLIV(44), XLV(45), XLVI(46), XLVII(47), XLVIII(48), XLIX(49), L(50),
+    LI(51), LII(52), LIII(53), LIV(54), LV(55), LVI(56), LVII(57), LVIII(58), LIX(59), LX(60),
+    LXI(61), LXII(62), LXIII(63), LXIV(64), LXV(65), LXVI(66), LXVII(67), LXVIII(68), LXIX(69), LXX(70),
+    LXXI(71), LXXII(72), LXXIII(73), LXXIV(74), LXXV(75), LXXVI(76), LXXVII(77), LXXVIII(78), LXXIX(79), LXXX(80),
+    LXXXI(81), LXXXII(82), LXXXIII(83), LXXXIV(84), LXXXV(85), LXXXVI(86), LXXXVII(87), LXXXVIII(88), LXXXIX(89), XC(90),
+    XCI(91), XCII(92), XCIII(93), XCIV(94), XCV(95), XCVI(96), XCVII(97), XCVIII(98), XCIX(99), C(100);
+    //Список римских чисел и соответствующее этому числу арабское число.
 
     public final int value;
 
